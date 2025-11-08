594e983fc4e7a9a7084c838aeb3e60ae7f9631ea
gpurci <gepurice@gmail.com>
2025-11-09 01:40:37 +0200

    add new selection method: 'turneu_choice', 'crescator', 'mixt'

--- laborator_1/GeneticAlgoritmMethods/my_code/select_parent.py
+++ laborator_1/GeneticAlgoritmMethods/my_code/select_parent.py
@@ -15,6 +15,7 @@ class SelectParent(RootGA):
     def __init__(self, config):
         super().__init__()
         self.setConfig(config)
+        self.parent_arg = 0
 
     def __call__(self):
         return self.fn()
@@ -22,18 +23,25 @@ class SelectParent(RootGA):
     def __config_fn(self):
         self.fn = self.selectParentAbstract
         if (self.__config is not None):
-            if   (self.__config == "choise"):
+            if   (self.__config == "choice"):
                 self.fn = self.selectParentChoice
             elif (self.__config == "roata"):
                 self.fn = self.selectParentWheel
             elif (self.__config == "turneu"):
                 self.fn = self.selectParentTour
+            elif (self.__config == "turneu_choice"):
+                self.fn = self.selectParentTourChoice
+            elif (self.__config == "crescator"):
+                self.fn = self.selectParentRise
+            elif (self.__config == "mixt"):
+                self.fn = self.selectParentMixt
+                
         else:
             pass
 
     def help(self):
         info = """SelectParent: 
-        metode de config: 'choise', 'roata', 'turneu'\n"""
+        metode de config: 'choice', 'roata', 'turneu', 'turneu_choice', 'crescator', 'mixt'\n"""
         return info
 
     def setConfig(self, config):
@@ -81,3 +89,39 @@ class SelectParent(RootGA):
         args_k_tour = np.random.randint(low=0, high=self.POPULATION_SIZE, size=7)
         arg         = np.argmax(self.fitness_values[args_k_tour])
         return args_k_tour[arg]
+
+    def selectParentTourChoice(self):
+        """Selecteaza un parinte aleator din populatie,
+            - unde valoarea fitness este probabilitatea de a fi ales
+        """
+        # selectie dupa compatibilitate, turneu
+        args_k_tour = np.random.choice(self.POPULATION_SIZE, size=7, p=self.fitness_values)
+        arg         = np.argmax(self.fitness_values[args_k_tour])
+        return args_k_tour[arg]
+
+    def selectParentRise(self):
+        """Selecteaza un parinte aleator din populatie,
+            - unde valoarea fitness este probabilitatea de a fi ales
+        """
+        # selectie dupa compatibilitate, in crestere
+        arg = self.parent_arg
+        self.parent_arg += 1
+        self.parent_arg = self.parent_arg % self.POPULATION_SIZE
+        return arg
+
+    def selectParentMixt(self):
+        """Selecteaza un parinte aleator din populatie,
+            - unde valoarea fitness este probabilitatea de a fi ales
+        """
+        # selectie dupa compatibilitate, mixt
+        cond = np.random.choice([0, 1, 2, 3], size=None, p=None)
+        if   (cond == 0):
+            arg = self.selectParentChoice()
+        elif (cond == 1):
+            arg = self.selectParentWheel()
+        elif (cond == 2):
+            arg = self.selectParentTour()
+        elif (cond == 3):
+            arg = self.selectParentRise()
+
+        return arg

13f034d189bd55a9159d3c26d3460cdda2706718
gpurci <gepurice@gmail.com>
2025-11-11 15:03:52 +0200

    update ttp generator

--- laborator_1/TTP/builds/ttp_generator.py
+++ laborator_1/TTP/builds/ttp_generator.py
@@ -1,79 +1,70 @@
 #!/usr/bin/python
 
-from pathlib import Path
 import numpy as np
 import cv2 as cv
 import pandas as pd
-import csv
-from math import hypot, ceil
 
 class TTPGenerator(object):
-    def __init__(self, nbr_city, max_distance, coord=None):
-        self.nbr_city     = nbr_city
-        self.max_distance = max_distance
-        if (coord is not None):
-            self.coord = coord
-        else:
-            self.coord = np.random.randint(low=0, high=self.max_distance, size=(self.nbr_city, 2))
+    def __init__(self, path):
+        self.path         = path
+        self.max_distance = 0
+        self.dataset      = None
+        self.coords       = None
 
-    def __call__(self):
-        return {"distance":self.distance, }
-
-    def __put_points(self, map):
-        for x, y in self.coord:
-            cv.circle(map, (x, y), 4, (0, 255, 0), -1)
-
-    def putRoutesOnMap(self, routes):
-        map = np.zeros((self.max_distance, self.max_distance, 3), dtype=np.uint8)
-        self.__put_points(map)
-
-        st_x, st_y = self.coord[routes[0]]
-        cv.circle(map, (st_x, st_y), 5, (255, 255, 255), 1)
-        for route in routes[1:]:
-            en_x, en_y = self.coord[route]
-            cv.circle(map, (st_x, st_y), 4, (0, 0, 255), -1)
-            cv.line(map,   (st_x, st_y), (en_x, en_y), (255, 0, 0), 4)
-            st_x, st_y = en_x, en_y
-        cv.circle(map, (st_x, st_y), 8, (0, 255, 255), 1)
-        return map
-
-    def read_csv(self, filename):
-        return pd.read_csv(filename, sep="\t", header=0)
-    
-    def load_ttp_csv(self, nodes_csv_path: str, items_csv_path: str):
-        df_nodes = pd.read_csv(nodes_csv_path, sep="\t")
-        coords = df_nodes[["X","Y"]].to_numpy(dtype=float)
-
-        n = coords.shape[0]
-        distance = self._pairwise_distance(ceil2d=True)
-
-        df_items = pd.read_csv(items_csv_path, sep="\t")
-        prof = np.zeros(n, dtype=float)
-        wgt  = np.zeros(n, dtype=float)
-
+    def __call__(self, nodes_file: str, items_file: str):
+        # citeste coordonatele oraselor
+        df_nodes = self.read_csv(nodes_file)
+        x = np.array(df_nodes["X"], dtype=np.int32)
+        y = np.array(df_nodes["Y"], dtype=np.int32)
+        self.coords = np.array(list(zip(x, y)), dtype=np.int32)
+        self.max_distance = int(self.coords.max()) + 10
+        # calculeaza distatele
+        distance = self._pairwise_distance(is_ceil2d=True)
+        # citeste greutatile si profitul
+        df_items = self.read_csv(items_file)
+        # initializare
+        prof = np.zeros(self.coords.shape[0], dtype=float)
+        wgt  = np.zeros(self.coords.shape[0], dtype=float)
+        # atribuire profit si greutate, pozitiilor din care fac parte
         for _, row in df_items.iterrows():
             city = int(row["ASSIGNED_NODE_NUMBER"]) - 1
             prof[city] = float(row["PROFIT"])
             wgt [city] = float(row["WEIGHT"])
+        # update dataset
+        self.dataset = {
+        "distance":    distance,
+        "coords":      self.coords,
+        "item_profit": prof,
+        "item_weight": wgt}
+        return self.dataset
 
-        self.coords      = coords
-        self.distance    = distance
-        self.item_profit = prof
-        self.item_weight = wgt
+    def __put_points(self, image):
+        for x, y in self.coords:
+            cv.circle(image, (x, y), 4, (0, 255, 0), -1)
 
+    def putRoutesOnMap(self, routes):
+        image = np.zeros((self.max_distance, self.max_distance, 3), dtype=np.uint8)
+        self.__put_points(image)
 
-    # utilitar numeric
-    def _is_number(self, x) -> bool:
-        try:
-            float(x); return True
-        except:
-            return False
+        st_x, st_y = self.coords[routes[0]]
+        cv.circle(image, (st_x, st_y), 5, (255, 255, 255), 1)
+        for route in routes[1:]:
+            en_x, en_y = self.coords[route]
+            cv.circle(image, (st_x, st_y), 4, (0, 0, 255), -1)
+            cv.line(image,   (st_x, st_y), (en_x, en_y), (255, 0, 0), 4)
+            st_x, st_y = en_x, en_y
+        cv.circle(image, (st_x, st_y), 8, (0, 255, 255), 1)
+        return image
 
+    def read_csv(self, filename):
+        filename = "{}/{}".format(self.path, filename)
+        return pd.read_csv(filename, sep="\t", header=0)
+    
     # calculeaza distante perechi, aplicand CEIL_2D (rotunjire în sus, nu distanta euclidiana reala)
     def _pairwise_distance(self, is_ceil2d: bool = True) -> np.ndarray:
         map_of_distance = []
-        for point in self.coord:
-            tmp_distance = np.linalg.norm(self.coord - point, axis=1)
+        for point in self.coords:
+            tmp_distance = np.linalg.norm(self.coords - point, axis=1)
             #print(tmp_distance)
             map_of_distance.append(tmp_distance)
         map_of_distance = np.array(map_of_distance)

ab16222f6528c8ed43072cc3cd85341cb06275f0
gpurci <gepurice@gmail.com>
2025-11-23 02:01:25 +0200

    update: tabu search pentru elita

--- extension/stres/my_code/stres_tsp.py
+++ extension/stres/my_code/stres_tsp.py
@@ -12,7 +12,9 @@ class StresTTP(StresBase):
     def __init__(self, method, dataset, freq_stres=10, subset_size=5, **configs):
         super().__init__(method, name="StresTTP", **configs)
         self.__fn = self._unpackMethod(method, 
+                                        normal=self.stres, 
                                         elite_tabu_search=self.stresTabuSearch, 
+                                        elite_tabu_search_by_distance=self.stresTabuSearchDistance, 
                                     )
         self.__score_evolution = np.zeros(subset_size, dtype=np.float32)
         self.dataset    = dataset
@@ -28,48 +30,57 @@ class StresTTP(StresBase):
 
     def help(self):
         info = """StresTTP:
+    metoda: 'normal';            config: -> None ;
     metoda: 'elite_tabu_search'; config: -> None ;
+    metoda: 'elite_tabu_search_by_distance'; config: -> None ;
     dataset    - setul de date de antrenare,
     freq_stres - frecventa cu care se va aplica stres,
     subset_size - esantionul de supraveghere\n"""
         print(info)
 
-    def stresTabuSearch(self, genoms, scores):
-        # unpack datassets
-        distance    = self.dataset["distance"]
-        item_profit = self.dataset["item_profit"]
-        item_weight = self.dataset["item_weight"]
-        # pack args
-        args = [distance, item_profit, item_weight]
+    def stres(self, genoms, scores):
+        """Aplica stres asupra populatiei.
+        Functia de stres, se aplica atunci cand ajungem intr-un minim local,
+        cauta cele mai frecvente secvente de genom si aplica un stres modifica acele zone
+        scores - scorul evolutiei
+        """
+        check_distance = np.allclose(self.__score_evolution.mean(), scores["score"], rtol=1e-01, atol=1e-03)
+        #print("distance evolution {}, distance {}".format(check_distance, best_distance))
 
+        # genoms.getElitePos() = returneaza pozitiile elite
+
+        if (check_distance):
+            self.__score_evolution[:] = 0
+            print("scores {}".format(scores))
+            # ADD STRESS FUNCTION
+        else:
+            self.__score_evolution[:-1] = self.__score_evolution[1:]
+            self.__score_evolution[-1]  = scores["score"]
+
+    def stresTabuSearch(self, genoms, scores):
         for elite_pos in genoms.getElitePos():
             # unpack elites
             individ = genoms[elite_pos]
+            # unpack route
+            route   = individ["tsp"]
             #print("route", route)
             # compute distance
-            best_score   = self.__computeIndividScore(individ, *args)
-            best_individ = individ.copy()
+            best_distance = self.computeIndividDistance(route)
+            best_route    = route.copy()
             # apply tabu search
             for locus1 in range(self.GENOME_LENGTH-1):
                 for locus2 in range(locus1+1, self.GENOME_LENGTH):
-                    tmp   = individ.copy()
-                    tmp["tsp"][locus1], tmp["tsp"][locus2] = tmp["tsp"][locus2], tmp["tsp"][locus1]
-                    score = self.__computeIndividScore(tmp, *args)
-                    if (score > best_score):
-                        best_score   = score
-                        best_individ = tmp
+                    tmp = route.copy()
+                    tmp[locus1], tmp[locus2] = tmp[locus2], tmp[locus1]
+                    distance = self.computeIndividDistance(tmp)
+                    if (distance < best_distance):
+                        best_distance = distance
+                        best_route    = tmp
             # set best route
-            genoms[elite_pos] = best_individ
+            genoms[elite_pos]["tsp"] = best_route
             #print("genoms", genoms[elite_pos]["tsp"])
 
     def stresTabuSearchDistance(self, genoms, scores):
-        # unpack datassets
-        distance    = self.dataset["distance"]
-        item_profit = self.dataset["item_profit"]
-        item_weight = self.dataset["item_weight"]
-        # pack args
-        args = [distance, item_profit, item_weight]
-
         # start tabu search by distance
         for elite_pos in genoms.getElitePos():
             # unpack elites
@@ -98,7 +109,6 @@ class StresTTP(StresBase):
             # set best route
             genoms[elite_pos]["tsp"] = best_route
 
-
     # ------------------ Utils ------------------
     def computeIndividDistance(self, individ):
         d = self.dataset["distance"]
@@ -109,46 +119,9 @@ class StresTTP(StresBase):
         city_distances = d[individ[:-1], individ[1:]]
         to_first_city  = d[individ[-1], individ[0]]
         return np.concatenate((city_distances, [to_first_city]))
+        """
 
-    def __computeScore(self, individ, *args):
-        # compute distance
-        profit, time, weight = self.__computeIndividAdaLiniar(individ, *args)
-        time = min_norm(time)
-        return (profit * time) / (profit + time)
-
-    #  TTP Liniar ---------------------
-    def __computeIndividScore(self, individ, *args, v_min=0.1, v_max=1, W=2000, alpha=0.01):
-        # unpack chromosomes
-        tsp_individ = individ["tsp"]
-        kp_individ  = individ["kp"]
-        # unpack args
-        # unpack datassets
-        distance, item_profit, item_weight = args
-        # obtine 
-        takes   = kp_individ[tsp_individ]
-        # calculare profit si greutate pentru fiecare oras
-        profits = item_profit[tsp_individ]*takes
-        weights = item_weight[tsp_individ]*takes
-        # calcularea distantelor dintre fiecare oras
-        city_d  = self.individCityDistance(tsp_individ)
-        city_d  = min_norm(city_d)
-        # calculare score
-        score = city_d * profits / (weights + 1e-7)
-        return score.sum()
-
-
-def normalization(x):
-    x_min = x.min()
-    x_max = x.max()
-    x_ret = (x_max-x)/(x_max-x_min+1e-7)
-    return x_ret
-
-def min_norm(x):
-    mask_not_zero = (x!=0)
-    valid_x = x[mask_not_zero]
-    if (valid_x.shape[0] > 0):
-        x_min = valid_x.min()
-    else:
-        x_min = 0.1
-        x[:] = 0.1
-    return (2*x_min)/(x+x_min)
+            def _cost(self, route):
+                d = self.dist
+                return d[route[:-1], route[1:]].sum() + d[route[-1], route[0]]
+        """

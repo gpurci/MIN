55cafee8d388bc7ab153324c5ff1bdeea49335fe
gpurci <gepurice@gmail.com>
2025-12-01 19:35:35 +0200

    update: fitness; add: linear, norm_linear, damped, exponential, norm_log

--- extension/fitness/my_code/fitness_ttp.py
+++ extension/fitness/my_code/fitness_ttp.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python
 
 import numpy as np
+from sys_function import sys_remove_modules
+sys_remove_modules("extension.utils.my_code.normalization")
+
 from extension.fitness.my_code.fitness_base import *
+from extension.utils.my_code.normalization import *
 
 class FitnessTTP(FitnessBase):
     """
@@ -12,32 +16,51 @@ class FitnessTTP(FitnessBase):
     def __init__(self, method, **configs):
         super().__init__(method, name="FitnessTTP", **configs)
         self.__fn = self._unpackMethod(method, 
-                                        TTP_f1score=self.fitnessF1scoreTTP, 
-                                        TTP=self.fitnessTTP,
+                                        f1score=self.f1score,
+                                        linear=self.linear,
+                                        norm_linear=self.norm_linear,
+                                        damped=self.damped,
+                                        exponential=self.exponential,
+                                        norm_log=self.norm_log
                                     )
 
     def __call__(self, metric_values):
         return self.__fn(metric_values, **self._configs)
 
     def help(self):
-        info = """Fitness:
-    metoda: 'TTP_f1score'; config: -> "R":1, ;
-    metoda: 'TTP';         config: -> "R":1, ;\n"""
+        info = """FitnessTTP:
+    metoda: 'f1score';     config: -> P_pres=1, W_pres=1, T_pres=1;
+    metoda: 'linear';      config: -> W_pres=1, R=1;
+    metoda: 'norm_linear'; config: -> W_pres=1, R=1;
+    metoda: 'damped';      config: -> W_pres=1, T_pres=1;
+    metoda: 'exponential'; config: -> W_pres=1, T_pres=1;
+    metoda: 'norm_log';    config: -> W_pres=1, T_pres=1;\n"""
         print(info)
 
     # TTP ------------------------------
-    def fitnessTTP(self, metric_values, R=1):
+    def f1score(self, metric_values, P_pres=1, W_pres=1, T_pres=1):
         """
-        Fitness cu decadere liniara.
-        Pentru fiecare individ:
-        - mergem pe traseu (route)
-        - cand ajungem intr-un oras luam obiectele de acolo
-        - profitul scade liniar cu timpul:
-                p(t) = p0 - alpha * t
-        - viteza scade in functie de greutatea acumulata
-        - costul de timp este penalizat cu R
-        Returneaza:
-            vector np.array cu fitness pentru fiecare individ
+        """
+        # unpack metrics
+        profits = metric_values["profits"]
+        weights = metric_values["weights"] # normalized
+        times   = metric_values["times"]
+        number_city = metric_values["number_city"]
+        number_obj  = metric_values["number_obj"]
+        # normalization
+        profits = normalization(profits)
+        profits = profits**P_pres
+        weights = weights**W_pres
+        times   = min_nonzeronorm(times)
+        times   = times**T_pres
+        mask_city = self.__cityBinarise(number_city)
+        number_obj= number_obj / self.GENOME_LENGTH
+        # calculate fitness
+        fitness = mask_city * ((number_obj * weights * profits * times) / (number_obj + weights + profits + times + 1e-7))
+        return fitness
+
+    def linear(self, metric_values, W_pres=1, R=1):
+        """
         """
         # unpack metrics
         profits = metric_values["profits"]
@@ -46,62 +69,87 @@ class FitnessTTP(FitnessBase):
         number_city = metric_values["number_city"]
         number_obj  = metric_values["number_obj"]
         # normalization
-        #profits = normalization(profits)
+        weights = weights**W_pres
+        mask_city = self.__cityBinarise(number_city)
+        number_obj= number_obj / self.GENOME_LENGTH
         # calculate fitness
-        fitness = (profits - R*times) * number_obj
-        #summary(profits=profits, weights=weights, times=times, fitness=fitness)
+        fitness = mask_city * number_obj * weights * (profits - R * times + 1e-7)
         return fitness
-    # TTP =================================
 
-    # TTP ------------------------------
-    def fitnessF1scoreTTP(self, metric_values, R=1):
+    def norm_linear(self, metric_values, W_pres=1, R=1): # To DO
         """
-        Fitness cu decadere liniara.
-        Pentru fiecare individ:
-        - mergem pe traseu (route)
-        - cand ajungem intr-un oras luam obiectele de acolo
-        - profitul scade liniar cu timpul:
-                p(t) = p0 - alpha * t
-        - viteza scade in functie de greutatea acumulata
-        - costul de timp este penalizat cu R
-        Returneaza:
-            vector np.array cu fitness pentru fiecare individ
         """
         # unpack metrics
-        profits     = metric_values["profits"]
-        times       = metric_values["times"]
-        weights     = metric_values["weights"]
+        profits = metric_values["profits"]
+        weights = metric_values["weights"]
+        times   = metric_values["times"]
         number_city = metric_values["number_city"]
         number_obj  = metric_values["number_obj"]
         # normalization
-        profits = normalization(profits)
-        times   = min_norm(times)
-        mask_city = self.__cityBinaryTSP(number_city)
+        weights = weights**W_pres
+        mask_city = self.__cityBinarise(number_city)
+        number_obj= number_obj / self.GENOME_LENGTH
         # calculate fitness
-        fitness = mask_city * number_obj * ((profits * times) / (profits + R*times+1e-7))
-        #fitness = number_obj * ((profits * times) / (profits + R*times+1e-7))
-        #summary(profits=profits, weights=weights, times=times, fitness=fitness)
+        tmp_linear = (profits - R * times + 1e-7)
+        linear_min = tmp_linear.min()
+        linear_max = tmp_linear.max()
+        norm_linear = (tmp_linear - linear_min) / (linear_max - linear_min)
+        fitness = mask_city * number_obj * weights * norm_linear
         return fitness
+
+    def damped(self, metric_values, W_pres=1, T_pres=1):
+        """
+        """
+        # unpack metrics
+        profits = metric_values["profits"]
+        weights = metric_values["weights"]
+        times   = metric_values["times"]
+        number_city = metric_values["number_city"]
+        number_obj  = metric_values["number_obj"]
+        # presure
+        weights = weights**W_pres
+        mask_city = self.__cityBinarise(number_city)
+        number_obj= number_obj / self.GENOME_LENGTH
+        # calculate fitness
+        fitness = mask_city * number_obj * weights * profits * np.exp(-T_pres * times)
+        return fitness
+
+    def exponential(self, metric_values, W_pres=1, T_pres=1):
+        """
+        """
+        # unpack metrics
+        profits = metric_values["profits"]
+        weights = metric_values["weights"]
+        times   = metric_values["times"]
+        number_city = metric_values["number_city"]
+        number_obj  = metric_values["number_obj"]
+        # normalization
+        weights = weights**W_pres
+        mask_city = self.__cityBinarise(number_city)
+        number_obj= number_obj / self.GENOME_LENGTH
+        # calculate fitness
+        fitness = mask_city * number_obj * weights * np.exp(profits - T_pres * times)
+        return fitness
+
+    def norm_log(self, metric_values, W_pres=1, T_pres=1):
+        """
+        """
+        # unpack metrics
+        profits = metric_values["profits"]
+        weights = metric_values["weights"]
+        times   = metric_values["times"]
+        number_city = metric_values["number_city"]
+        number_obj  = metric_values["number_obj"]
+        # normalization
+        weights = weights**W_pres
+        mask_city = self.__cityBinarise(number_city)
+        number_obj= number_obj / self.GENOME_LENGTH
+        # calculate fitness
+        fitness = mask_city * number_obj * weights * np.exp(profits) / (1 + np.exp(T_pres * times))
+        return fitness
+
     # TTP =================================
 
-def summary(**kw):
-    print("Summary")
-    for name in kw.keys():
-        val = kw[name]
-        print("{}: min {}, max {}, mean {}, std {}".format(name, val.min(), val.max(), np.mean(val), np.std(val)))
-
-def normalization(x):
-    x_min = x.min()
-    x_max = x.max()
-    x_ret = (x_max-x)/(x_max-x_min+1e-7)
-    return x_ret
-
-def min_norm(x):
-    mask_not_zero = (x!=0)
-    valid_x = x[mask_not_zero]
-    if (valid_x.shape[0] > 0):
-        x_min = valid_x.min()
-    else:
-        x_min = 0.1
-        x[:] = 0.1
-    return (2*x_min)/(x+x_min)
+    def __cityBinarise(self, number_city):
+        mask_cities = (number_city>=self.GENOME_LENGTH).astype(np.float32)
+        return mask_cities

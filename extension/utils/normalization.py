6c729f9b38269ccc77bac9f7dc771f3ea348ed58
gpurci <gepurice@gmail.com>
2025-12-06 01:36:09 +0200

    add: adauga normalizare si standardizare dupa referinta; calculeaza statistati metricilor

--- extension/utils/normalization.py
+++ extension/utils/normalization.py
@@ -21,3 +21,43 @@ def min_nonzeronorm(x):
     else:
         x_min = 1
     return (2*x_min)/(x+x_min)
+
+def min_nonzero(x):
+    mask_not_zero = (x!=0)
+    valid_x = x[mask_not_zero]
+    if (valid_x.shape[0] > 0):
+        x_min = valid_x.min()
+    else:
+        x_min = 1
+    return x_min
+
+def normalization_reference(x, x_min, x_max):
+    denom = x_max - x_min
+    if (denom == 0):
+        # all values are identical: return a constant vector (e.g. all 1s)
+        x_ret = np.ones_like(x, dtype=np.float32)
+    else:
+        x_ret = (x-x_min)/denom
+    return x_ret
+
+def min_nonzeronorm_reference(x, x_min):
+    return (2*x_min)/(x+x_min)
+
+def min_nonzero_values(metric_values, prev_metric_values, key):
+    tmp      = metric_values[key]
+    tmp_prev = prev_metric_values[key]
+    tmp = np.concatenate((tmp, tmp_prev), axis=None)
+    return min_nonzero(tmp)
+
+def normal_values(metric_values, prev_metric_values, key):
+    tmp      = metric_values[key]
+    tmp_prev = prev_metric_values[key]
+    tmp = np.concatenate((tmp, tmp_prev), axis=None)
+    return tmp.min(), tmp.max()
+
+def standart_values(metric_values, prev_metric_values, key):
+    tmp      = metric_values[key]
+    tmp_prev = prev_metric_values[key]
+    tmp = np.concatenate((tmp, tmp_prev), axis=None)
+    return tmp.mean(), tmp.std()
+
--- extension/utils/standardization.py
+++ extension/utils/standardization.py
@@ -5,5 +5,7 @@ import numpy as np
 def standardization(x):
     x_mean = x.mean()
     x_std  = x.std()
-    return (x - x_mean)/x_std
+    return (x - x_mean) / x_std
 
+def standardization_reference(x, x_mean, x_std):
+    return (x - x_mean) / x_std

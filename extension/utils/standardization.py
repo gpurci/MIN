c46f59fe2af40742bf92298720080484334a6957
gpurci <gepurice@gmail.com>
2025-12-05 20:29:00 +0200

    update: sterge fisiere invechite

--- extension/utils/standardization.py
+++ extension/utils/standardization.py
@@ -6,3 +6,4 @@ def standardization(x):
     x_mean = x.mean()
     x_std  = x.std()
     return (x - x_mean)/x_std
+
--- laborator_1/TTP/builds/fitness/fitness_ttp_v2.py
+++ laborator_1/TTP/builds/fitness/fitness_ttp_v2.py
@@ -9,14 +9,14 @@ from extension.fitness.fitness_base import *
 from extension.utils.normalization import *
 from extension.utils.standardization import *
 
-class FitnessTTPV1(FitnessBase):
+class FitnessTTPV2(FitnessBase):
     """
-    Clasa 'FitnessTTPV1', ofera doar metode pentru a calcula functia fitness din populatie.
+    Clasa 'FitnessTTPV2', ofera doar metode pentru a calcula functia fitness din populatie.
     Functia 'fitness' are 1 parametru, numarul populatiei.
     Pentru o configuratie inexistenta, vei primi un mesaj de eroare.
     """
     def __init__(self, method, **configs):
-        super().__init__(method, name="FitnessTTPV1", **configs)
+        super().__init__(method, name="FitnessTTPV2", **configs)
         self.__fn = self._unpackMethod(method, 
                                         f1score=self.f1score,
                                         linear=self.linear,
@@ -29,7 +29,7 @@ class FitnessTTPV1(FitnessBase):
         return self.__fn(metric_values, **self._configs)
 
     def help(self):
-        info = """FitnessTTPV1:
+        info = """FitnessTTPV2:
     metoda: 'f1score';     config: -> P_pres=1, W_pres=1, T_pres=1, D_pres=1;
     metoda: 'linear';      config: -> W_pres=1, D_pres=1, R=1;
     metoda: 'norm_linear'; config: -> W_pres=1, D_pres=1, R=1;
@@ -78,7 +78,10 @@ class FitnessTTPV1(FitnessBase):
         mask_city = self.__cityBinarise(number_city)
         #number_obj= number_obj / self.GENOME_LENGTH
         # calculate fitness
-        fitness = mask_city * (profits - R * times + 1e-7)
+        tmp_score = profits - R * times
+        min_score = tmp_score.min()
+        tmp_score -= min_score
+        fitness = mask_city * tmp_score
         return fitness
 
     def norm_linear(self, metric_values, P_pres=1, W_pres=1, T_pres=1, D_pres=1, R=1):
@@ -98,11 +101,11 @@ class FitnessTTPV1(FitnessBase):
         mask_city = self.__cityBinarise(number_city)
         number_obj= number_obj / self.GENOME_LENGTH
         # calculate fitness
-        tmp_linear = (profits - R * times + 1e-7)
-        linear_min = tmp_linear.min()
-        linear_max = tmp_linear.max()
-        norm_linear = (tmp_linear - linear_min) / (linear_max - linear_min + 1e-7)
-        fitness = mask_city * number_obj * weights * distances * norm_linear / (weights + distances + norm_linear)
+        tmp_score = profits - R * times
+        tmp_score = standardization(tmp_score)
+        min_score = tmp_score.min()
+        tmp_score -= min_score
+        fitness = mask_city * number_obj * weights * distances * tmp_score / (weights + distances)
         return fitness
 
     def mixt(self, metric_values, p_select=None, **kw):

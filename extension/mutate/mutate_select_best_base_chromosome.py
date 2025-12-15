c6266d5753365388aec3ad58db2d88b82f873f91
gpurci <gepurice@gmail.com>
2025-12-15 18:02:07 +0200

    update: select best mutate

--- extension/mutate/mutate_select_best_base_chromosome.py
+++ extension/mutate/mutate_select_best_base_chromosome.py
@@ -6,9 +6,9 @@ from extension.ga_select_best_base import *
 class MutateSelectBestChromosome(GASelectBestBase):
     """
     """
-    def __init__(self, metrics, *objects):
+    def __init__(self, best_fn, *objects):
         super().__init__(*objects, name="MutateSelectBest", inherit_class="MutateBase")
-        self.metrics = metrics
+        self.best_fn = best_fn
 
     def __call__(self, parent1, parent2, offspring):
         cond = np.random.choice(self._range, size=None, p=self_p_select)
@@ -16,5 +16,5 @@ class MutateSelectBestChromosome(GASelectBestBase):
         new_offspring = np.repeat(offspring.reshape(1, -1), size+1, axis=0)
         for idx in range(size):
             new_offspring[idx] = self._objects[idx](parent1, parent2, offspring)
-        best_idx = self.metrics.
+        best_idx = self.best_fn(new_offspring)
         return 
